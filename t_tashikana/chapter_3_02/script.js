39e84b8097bc5d39597b0450b73a49007d39f052
KatTakahashi <92353507+KatTakahashi@users.noreply.github.com>
2022-02-17 16:29:48 +0900

    [Finish]chapter_3_2

---
+++ t_tashikana/chapter_3_02/script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// let answer = window.prompt('ヘルプを見ますか');
+// console.log(answer);
+
+// const answer = window.prompt('ヘルプをみますか');
+// console.log(answer);
+// answer = 'no';
+// console.log(answer);
+
+// const answer = window.prompt('ヘルプをみますか');
+// console.log(answer);
+
+const answer = window.prompt('ヘルプを見ますか');
+if (answer === 'yes'){
+  window.alert('タップでジャンプ、障害物をよけます。');
+}
