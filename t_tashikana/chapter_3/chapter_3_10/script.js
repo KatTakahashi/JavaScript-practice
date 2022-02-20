2b4f790db6c54af44e69ce5b5f7354592171cb3c
KatTakahashi <92353507+KatTakahashi@users.noreply.github.com>
2022-02-20 14:53:24 +0900

    [Finish]chapter_3-10

--- t_tashikana/chapter_3/chapter_3_10/script.js
+++ t_tashikana/chapter_3/chapter_3_10/script.js
@@ -1,4 +1,16 @@
 'use strict';
 
 let todo = ['デザインカンプ作成', 'データ整理', '勉強申込み', '牛乳買う'];
-console.log(todo[0]);
+todo.push('歯医者に行く');
+
+// console.log(todo[4]);
+
+// for(let item of todo){
+//   console.log(item);
+// }
+
+for(let item of todo){
+  const li = `<li>${item}</li>`;
+  // console.log(li);
+  document.getElementById('list').insertAdjacentHTML('beforeend', li);
+}
